7bd8818795e5e81e114da61abe69f57edbda2fef
Mateusz Gliwa <mateuszgliwa.all@gmail.com>
2022-01-22 13:48:49 +0100

    Space Invader

--- spaceInvader.py
+++ spaceInvader.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+from pygame import mixer
 
 # initialize pygame
 pygame.init()
@@ -10,7 +11,8 @@ screen = pygame.display.set_mode((800, 600))
 
 # BG
 bg = pygame.image.load("bg.jpg")
-
+mixer.music.load("bg.wav")
+mixer.music.play(-1)
 # Personalize display
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("spaceship.png")
@@ -23,12 +25,18 @@ playerY = 480
 playerX_change = 0
 
 # Alien
-
-alienIMG = pygame.image.load("alien.png")
-alienX = random.randint(0, 734)
-alienY = random.randint(30, 150)
-alienX_change = 0.1
-alienY_change = 40
+alienIMG = []
+alienX = []
+alienY = []
+alienX_change = []
+alienY_change = []
+alienNum = 6
+for i in range(alienNum):
+    alienIMG.append(pygame.image.load("alien.png"))
+    alienX.append(random.randint(0, 734))
+    alienY.append(random.randint(30, 150))
+    alienX_change.append(0.1)
+    alienY_change.append(40)
 
 # Rocket
 rocketIMG = pygame.image.load("rocket.png")
@@ -37,15 +45,32 @@ rocketY = 480
 rocketY_change = 0.3
 rocket_state = "ready"
 
+# Score
 score = 0
+font = pygame.font.Font("Starjedi.ttf", 32)
+textX = 10
+textY = 10
+
+# game over
+over_font = pygame.font.Font("Starjedi.ttf", 64)
+
+
+def game_over():
+    over_txt = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_txt, (200,250))
+
+
+def show_score(x, y):
+    score_font = font.render("Score : " + str(score), True, (255, 255, 255))
+    screen.blit(score_font, (10, 10))
 
 
 def player(x, y):
     screen.blit(playerIMG, (x, y))
 
 
-def alien(x, y):
-    screen.blit(alienIMG, (x, y))
+def alien(x, y, i):
+    screen.blit(alienIMG[i], (x, y))
 
 
 def fire_rocket(x, y):
@@ -79,7 +104,9 @@ while running:
             if event.key == pygame.K_d:
                 playerX_change = 0.2
             if event.key == pygame.K_SPACE:
-                if rocket_state is "ready":
+                if rocket_state == "ready":
+                    rocket_sound = mixer.Sound("laser.wav")
+                    rocket_sound.play()
                     rocketX = playerX
                     fire_rocket(rocketX, rocketY)
 
@@ -93,33 +120,41 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
-    alienX += alienX_change
-    if alienX <= 0:
-        alienX_change = 0.1
-        alienY += alienY_change
-    elif alienX >= 736:
-        alienX_change = -0.1
-        alienY += alienY_change
+    for i in range(alienNum):
 
+        # game over
+        if alienY[i] > 440:
+            for j in range(alienNum):
+                alienY[j] = 2000
+            game_over()
+            break
+
+        alienX[i] += alienX_change[i]
+        if alienX[i] <= 0:
+            alienX_change[i] = 0.1
+            alienY[i] += alienY_change[i]
+        elif alienX[i] >= 736:
+            alienX_change[i] = -0.1
+            alienY[i] += alienY_change[i]
+        # Collision
+        collision = isCollision(alienX[i], alienY[i], rocketX, rocketY)
+        if collision:
+            rocketY = 480
+            rocket_state = "ready"
+            score += 1
+            kill_sound = mixer.Sound("kill.wav")
+            kill_sound.play()
+            alienX[i] = random.randint(0, 734)
+            alienY[i] = random.randint(30, 150)
+        alien(alienX[i], alienY[i], i)
     # rocket shoot
     if rocketY <= 0:
         rocketY = 480
         rocket_state = "ready"
-    if rocket_state is "fire":
+    if rocket_state == "fire":
         fire_rocket(rocketX, rocketY)
         rocketY -= rocketY_change
 
-    # Collision
-    collision = isCollision(alienX, alienY, rocketX, rocketY)
-    if collision:
-        rocketY = 480
-        rocket_state = "ready"
-        score += 1
-        print(score)
-        alienX = random.randint(0, 734)
-        alienY = random.randint(30, 150)
-
     player(playerX, playerY)
-    alien(alienX, alienY)
-
+    show_score(textX, textY)
     pygame.display.update()
